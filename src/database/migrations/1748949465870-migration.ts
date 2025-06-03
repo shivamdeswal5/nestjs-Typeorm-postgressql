5e4ac1218206c48da8cbdcefaa4f5a41a2e47209
Shivam Deswal <deswalworks@gmail.com>
2025-06-03 17:36:13 +0530

    migrations

--- src/database/migrations/1748949465870-migration.ts
+++ src/database/migrations/1748949465870-migration.ts
@@ -5,6 +5,7 @@ export class Migration1748949465870 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "group" ("id" SERIAL NOT NULL, "name" character varying(30) NOT NULL, "ceo" character varying(15) NOT NULL, "email" character varying(40) NOT NULL, CONSTRAINT "UQ_fa6828b123cdc1d92e1b72fde91" UNIQUE ("email"), CONSTRAINT "PK_256aa0fda9b1de1a73ee0b7106b" PRIMARY KEY ("id"))`);
+        
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
---
+++ src/database/migrations/1748950667761-update-group-migration.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UpdateUserTransaction1748950667761 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "group" DROP COLUMN "ceo"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "group"`);
+    }
+
+}
